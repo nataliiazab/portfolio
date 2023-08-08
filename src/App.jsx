e092aeb696c99559e1722364b94efef5b3385b0a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-09 00:09:09 +0100

    Complete Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <>
-      <h1>Hello There!</h1>
+     <Header />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,50 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-
+import React from "react";
+//logo image
+import logo from "../assets/profile-pic.png";
+import { Link } from "react-scroll";
 const Header = () => {
+  let Links = [
+    { name: "Home", link: "home" },
+    { name: "About", link: "about" },
+    { name: "Projects", link: "projects" },
+    { name: "Contact", link: "contact" },
+  ];
   return (
-    <div>
-      Header
+    <div className="w-full relative z-[100]">
+      <div className="md:flex items-center justify-between bg-white py-4 md:py-0 md:px-14 px-7 fixed top-0 left-0 right-0 shadow-sm">
+        {/*logo*/}
+        <div className="font-bold text-2xl tracking-wide cursor-pointer flex items-center gap-1">
+          <img
+            src={logo}
+            alt="illustration of a woman who code"
+            className="md:w-12 md:h-12 w-10 h-10"
+          />
+          <span className="text-primary hover:text-secondary md:ml-3 ml-2">
+            NATALIE ZABLOTSKA
+          </span>
+        </div>
+
+        {/*nav items*/}
+        <ul className="md:flex md:items-center items center md:pb-0 absolute md:static md:z-auto z-50 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 bg-white sm:bg-transparent">
+          {Links.map((link) => (
+            <li
+              key={link.name}
+              className="md-ml-8 md:my-8 mt-5 font-semibold text-xl border-b-2 md:border-b-0 text-right px-7 md:px-4 pb-4 md:pb-0 cursor-pointer"
+            >
+              <Link
+                to={link.link}
+                className="text-light hover:text-secondary duration-500 uppercase"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Header;
+//22
