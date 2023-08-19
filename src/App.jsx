3642895e299708bde9c668af81f7baa8f3bb9aa3
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-19 23:01:52 +0100

    add Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
-
-
-
-
-
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
-     <Header />
-     <Home /> 
-     <About />
+      <Header />
+      <Home />
+      <About />
+      <Projects />
     </>
   );
 }
