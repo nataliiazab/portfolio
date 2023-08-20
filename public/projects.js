4ce3519ce7ee1056318fd3628537adfa7088e11a
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-20 21:52:44 +0100

    add my projects

--- public/projects.js
+++ public/projects.js
@@ -1,8 +1,32 @@
 const projects = [
-    {id: 1, name: 'Dopefolio', description: 'Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally', image: '/src/assets/dopefolio.jpeg'},
-    {id: 2, name: 'Wilsonport', description: 'Wilsonport is a multiservice logistics and transport company and I created their website from scratch using the frontend tools I know.', image: '/src/assets/wilsonport.jpeg'},
-    {id: 3, name: 'Boreal Coffee Clone', description: "I re-created the frontend of Boreal Coffee's official web app because I got attracted to their beautiful UI. It was a great experience for me to build the entire frontend.", image: '/src/assets/boreal-coffee-clone.jpeg'},
-    {id: 4, name: 'Crown Template', description:'Crown is a web template that I created targeting the restaurant and food industry which anyone can use to present their business online.', image:'/src/assets/crown-template.jpeg'}
-]
+  {
+    id: 1,
+    name: "Good PR App",
+    description:
+      "The Good PR App is a final group project for Code Your Future (CYF) using Next.js, Prisma, and Tailwind CSS. I collaborated with 3 other developers to create this App",
+    image: "/src/assets/good-pr.png",
+  },
+  {
+    id: 2,
+    name: "Country Explorer",
+    description:
+      "Country Explorer is a React web application that allows users to explore information about different countries around the world. It provides features such as country search, region-based filtering, and a toggle for light and dark mode.",
+    image: "/src/assets/Country-Explorer.png",
+  },
+  {
+    id: 3,
+    name: "Barbie Movie Quiz App",
+    description:
+      "Engineered an interactive React app matching users' traits with Barbie movie characters. Utilised third-party libraries and responsive design for optimal user experience",
+    image: "/src/assets/barbie-app.png",
+  },
+  {
+    id: 4,
+    name: "Rock, Paper, Scissors Game",
+    description:
+      "I've built a simple Rock, Paper, Scissors game built using HTML, CSS, JavaScript and DOM. The game allows two players to enter their names, make their choices, and see the winner's result",
+    image: "/src/assets/Rock-Paper-Scissors-Game.png",
+  },
+];
 
 export default projects;
