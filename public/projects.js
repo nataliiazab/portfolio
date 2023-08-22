296b174a72be59910354ed744e8a4962d92d10bf
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-22 21:25:41 +0100

    move project images to public folder

--- public/projects.js
+++ public/projects.js
@@ -4,28 +4,28 @@ const projects = [
     name: "Good PR App",
     description:
       "The Good PR App is a final group project for Code Your Future bootcamp using Next.js, Prisma, and Tailwind CSS. I collaborated with 3 other developers to create this App",
-    image: "./src/assets/good-pr.png",
+    image: "./images/good-pr.png",
   },
   {
     id: 2,
     name: "Country Explorer",
     description:
       "Country Explorer is a React web application that allows users to explore information about different countries around the world. It provides features such as country search, region-based filtering, and a toggle for light and dark mode.",
-    image: "./src/assets/Country-Explorer.png",
+    image: "./images/Country-Explorer.png",
   },
   {
     id: 3,
     name: "Barbie Movie Quiz App",
     description:
       "Engineered an interactive React app matching users' traits with Barbie movie characters. Utilised third-party libraries and responsive design for optimal user experience",
-    image: "./src/assets/barbie-app.png",
+    image: "./images/barbie-app.png",
   },
   {
     id: 4,
     name: "Rock, Paper, Scissors Game",
     description:
       "I've built a simple Rock, Paper, Scissors game built using HTML, CSS, JavaScript and DOM. The game allows two players to enter their names, make their choices, and see the winner's result",
-    image: "./src/assets/Rock-Paper-Scissors-Game.png",
+    image: "./images/Rock-Paper-Scissors-Game.png",
   },
 ];
 
