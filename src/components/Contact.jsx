b469c7a4d97fc307a857716765ae8a7ab5bad72f
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-22 21:39:42 +0100

    complete Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,51 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import Headline from "../shared/Headline";
 
 const Contact = () => {
-    return (
-        <div>
-            Contact
-        </div>
-    );
+  return (
+    <div className="contact mx-auto mt-8 px-7">
+      <Headline
+        title={"CONTACT"}
+        subtitle={
+          "Feel free to Contact me by submitting the form below and I will get back tou you as soon as possible"
+        }
+      />
+      <div className="md:w-2/3 mx-auto bg-white md:px-16 px-8 py-8 rounded mb-32">
+        <form>
+          <label htmlFor="">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Enter Your Name"
+            className="p-5"
+          ></input>
+
+          <label htmlFor="">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Your Email"
+            className="p-5"
+          ></input>
+
+          <label htmlFor="">Message:</label>
+          <textarea
+            name="message"
+            id="message"
+            cols="80"
+            rows="10"
+            placeholder="Enter Your Message"
+            className="p-5 mb-8"
+          ></textarea>
+
+          <button className="btn px-14 py-4 shadow-sm">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
