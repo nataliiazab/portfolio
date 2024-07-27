b5334d9ffc3fc613500ecdd746dfc6214e6290dd
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-07-27 19:06:38 +0100

    Update skills.js

--- public/skills.js
+++ public/skills.js
@@ -1,13 +1,24 @@
 const skills = [
-  { id: 1, title: "HTML" },
-  { id: 2, title: "CSS" },
+  { id: 1, title: "HTML5" },
+  { id: 2, title: "CSS3" },
   { id: 3, title: "JavaScript" },
-  { id: 4, title: "React" },
+  { id: 4, title: "React.js" },
   { id: 5, title: "Next.js" },
   { id: 6, title: "Node.js" },
   { id: 7, title: "Tailwind CSS" },
-  { id: 8, title: "SQL" },
-  { id: 9, title: "Git/GitHub" }
+  { id: 8, title: "Bootstrap" },
+  { id: 9, title: "PostgreSQL" },
+  { id: 10, title: "Postman" },
+  { id: 11, title: "Python" },
+  { id: 12, title: "Linux" },
+  { id: 13, title: "Git" },
+  { id: 14, title: "GitHub" },
+  { id: 15, title: "REST APIs" },
+  { id: 16, title: "Agile" },
+  { id: 17, title: "Supabase" },
+  { id: 18, title: "Prisma" },
+  { id: 19, title: "Jest" },
+  { id: 20, title: "Docker" },
 ];
 
 export default skills;
